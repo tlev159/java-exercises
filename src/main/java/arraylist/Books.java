7b5eb09261475251e673d3a2466d5a552e7eacf2
tlev159 <t.levente1598@gmail.com>
2020-11-15 16:47:03 +0100

    arraylist Books bonus2

--- src/main/java/arraylist/Books.java
+++ src/main/java/arraylist/Books.java
@@ -4,6 +4,7 @@ import classstructureconstructors.Book;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Books {
@@ -28,6 +29,17 @@ public class Books {
     return bookTitles;
   }
 
+  public void removeByPrefix(String prefix) {
+      List<String> toDeleteBookTitles = new ArrayList<>();
+      for (String title : bookTitles) {
+        if (title.indexOf(prefix) == 0) {
+          toDeleteBookTitles.add(title);
+        }
+      }
+      bookTitles.removeAll(toDeleteBookTitles);
+  }
+
+
   public static void main(String[] args) {
     Books books = new Books();
 
@@ -38,5 +50,9 @@ public class Books {
 
     System.out.println(books.findAllByPrefix("Add").toString());
     System.out.println(books.getTitles().toString());
+
+    books.removeByPrefix("Add");
+    System.out.println(books.getTitles().toString());
+
   }
 }
