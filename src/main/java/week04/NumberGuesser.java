91ec71708eeb911c93990029b3ffb7b28ba7cb30
tlev159 <t.levente1598@gmail.com>
2020-11-18 14:07:25 +0100

    week04 NumberGuesser

---
+++ src/main/java/week04/NumberGuesser.java
@@ -0,0 +1,36 @@
+package week04;
+
+import java.sql.SQLOutput;
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuesser {
+
+
+
+  public static void main(String[] args) {
+    Random rnd = new Random();
+    int guessedNumber = rnd.nextInt(100) + 1;
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Kitaláltam egy számot 1 és 100 között!");
+    System.out.println("Találd ki, melyik számra gondoltam!");
+    int number = 0;
+    int round = 0;
+    while (guessedNumber != number && round < 7) {
+      round++;
+      System.out.println(round + ". kör a 7-ből");
+      number = scanner.nextInt();
+      if (guessedNumber > number) {
+        System.out.println("A kitalált szám ennél nagyobb!");
+      } else if (guessedNumber < number){
+        System.out.println("A kitalált szám ennél kisebb!");
+      } else System.out.println("Kitaláltad!");
+    }
+
+    System.out.println("A gondolt szám: " + guessedNumber);
+
+
+  }
+}
