b857cdf8e676023e859ae249f4880de13573859c
tlev159 <t.levente1598@gmail.com>
2020-11-19 16:55:25 +0100

    introjunit Gentleman and Test

--- src/main/java/introjuit/Gentleman.java
+++ src/main/java/introjuit/Gentleman.java
@@ -3,6 +3,9 @@ package introjuit;
 public class Gentleman {
 
   public String sayHello(String name) {
+    if (name == null) {
+      return "Hello Anonymus";
+    }
     return "Hello " + name;
   }
 }
--- src/test/java/introjunit/GentlemanTest.java
+++ src/test/java/introjunit/GentlemanTest.java
@@ -10,10 +10,13 @@ public class GentlemanTest {
 
   @Test
   public void testSayHello() {
-    Gentleman gentleman = new Gentleman();
-    String answer = gentleman.sayHello("John Doe");
-    assertEquals("Hello John Doe", answer);
+    String answer = "John Doe";
+    String gentleman = new Gentleman().sayHello(answer);
+    assertEquals("Hello John Doe", gentleman);
 
+
+    String answer1 = null;
+    String gentleman2 = new Gentleman().sayHello(answer1);
+    assertEquals("Hello Anonymus", gentleman2);
   }
-
 }
