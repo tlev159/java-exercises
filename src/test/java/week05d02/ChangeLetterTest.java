051703721f94ad2e5599e451212dc43289dda1ae
tlev159 <t.levente1598@gmail.com>
2020-11-24 15:29:57 +0100

    week05d02.Changeletter tesztesetek

---
+++ src/test/java/week05d02/ChangeLetterTest.java
@@ -0,0 +1,22 @@
+package week05d02;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class ChangeLetterTest {
+
+  @Test
+  public void testChangeLetter() {
+    ChangeLetter cL = new ChangeLetter();
+    String changeLetterTest1 = "arvizturotukorfurogep";
+    assertEquals("*rv*zt*r*t*k*rf*r*g*p", cL.changeVowels(changeLetterTest1));
+  }
+
+  @Test
+  public void testChangeLetterTestCaseSensitive2() {
+    ChangeLetter cL = new ChangeLetter();
+    String changeLetterTest2 = "Halado holdado Elment Uberrel";
+    assertEquals("H*l*d* h*ld*d* *lm*nt *b*rr*l", cL.changeVowels(changeLetterTest2));
+  }
+}
