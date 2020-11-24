c8484c5e81f3dfd356c932c71228910a99b7dead
tlev159 <t.levente1598@gmail.com>
2020-11-24 20:01:04 +0100

    introexception.patient Patient and PatientTest

---
+++ src/main/java/introexception/patient/Patient.java
@@ -0,0 +1,34 @@
+package introexception.patient;
+
+public class Patient {
+
+  private String name;
+  private String socialSecurityNumber;
+  private int yearOfBirth;
+
+  public Patient(String name, String socialSecurityNumber, int yearOfBirth) {
+    try {
+      int validInt = Integer.parseInt(socialSecurityNumber);
+    } catch (IllegalArgumentException iae) {
+      throw new IllegalArgumentException("Nem érvényes a TAJ-szám");
+    }
+      if (name == null || name.isEmpty() || yearOfBirth < 1900) {
+      throw new IllegalArgumentException();
+    }
+    this.name = name;
+    this.socialSecurityNumber = socialSecurityNumber;
+    this.yearOfBirth = yearOfBirth;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getSocialSecurityNumber() {
+    return socialSecurityNumber;
+  }
+
+  public int getYearOfBirth() {
+    return yearOfBirth;
+  }
+}
