2666c03c1d8fc4ffc962dd9392ab5c17616fcf60
tlev159 <t.levente1598@gmail.com>
2020-11-25 21:20:11 +0100

    week05d03 changing the codes as given, it gives exception if founded the first wrong data

--- src/test/java/week05d03/UsersValidatorTest.java
+++ src/test/java/week05d03/UsersValidatorTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+
 public class UsersValidatorTest {
 
   @Test
