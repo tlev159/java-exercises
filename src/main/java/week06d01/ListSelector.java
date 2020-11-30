eb1e64fc2e97fab703b125f73d1897f4137d16ea
tlev159 <t.levente1598@gmail.com>
2020-11-30 15:46:37 +0100

    list selector without bonus and test

---
+++ src/main/java/week06d01/ListSelector.java
@@ -0,0 +1,31 @@
+package week06d01;
+
+import java.util.List;
+
+public class ListSelector {
+
+  public String selectFromList(List<String> stringList) {
+    try {
+      StringBuilder sb = new StringBuilder();
+      for (int i = 0; i < stringList.size(); i+=2) {
+        sb.append(stringList.get(i));
+      }
+      return "[" + sb.toString() + "]";
+    } catch (IllegalArgumentException ie) {
+      throw new IllegalArgumentException("Empty String list!");
+    }
+  }
+
+//  public String selectFromListWith(List<String> stringList) {
+//    try {
+//      StringBuilder sb = new StringBuilder();
+//      for (int i = 0; i < stringList.size() - 2; i+=2) {
+//        sb.append(stringList.get(i)).append(", ");
+//      }
+//      sb.append(stringList.get());
+//      return "[" + sb.toString() + "]";
+//    } catch (IllegalArgumentException ie) {
+//      throw new IllegalArgumentException("Empty String list!");
+//    }
+//  }
+}
