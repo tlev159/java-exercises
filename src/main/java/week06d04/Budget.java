49ff897c3dd7ac6b9d08b14830be000df76c6e82
tlev159 <t.levente1598@gmail.com>
2020-12-03 15:53:09 +0100

    Item and Budget without Test

---
+++ src/main/java/week06d04/Budget.java
@@ -0,0 +1,19 @@
+package week06d04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Budget {
+
+  List<Item> items = new ArrayList<>();
+
+  public List getItemsByMonth(int month) {
+    List<Item> buydInGivenMonth = new ArrayList<>();
+    for (Item i : items) {
+      if (i.getDate().getMonthValue() == month) {
+        buydInGivenMonth.add(i);
+      }
+    }
+    return buydInGivenMonth;
+  }
+}
---
+++ src/main/java/week06d04/Item.java
@@ -0,0 +1,28 @@
+package week06d04;
+
+import java.time.LocalDate;
+
+public class Item {
+
+  private int price;
+  private LocalDate date;
+  private String name;
+
+  public Item(int price, LocalDate date, String name) {
+    this.price = price;
+    this.date = date;
+    this.name = name;
+  }
+
+  public int getPrice() {
+    return price;
+  }
+
+  public LocalDate getDate() {
+    return date;
+  }
+
+  public String getName() {
+    return name;
+  }
+}
