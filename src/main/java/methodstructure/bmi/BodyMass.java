5a01d40a2ed5c5fb40c0419ad2ed1a7ef4052011
tlev159 <t.levente1598@gmail.com>
2020-12-04 17:27:47 +0100

    BMI calculator that gives back if the bmi for the given weight and height underweight, normal or overweight

---
+++ src/main/java/methodstructure/bmi/BodyMass.java
@@ -0,0 +1,38 @@
+package methodstructure.bmi;
+
+public class BodyMass {
+
+  private double weight;
+  private double height;
+
+  public BodyMass(double weight, double height) {
+    this.weight = weight;
+    this.height = height;
+  }
+
+  public double bodyMassIndex() {
+    return this.weight / Math.pow(this.height, 2);
+  }
+
+  public BmiCategory body() {
+    if (bodyMassIndex() < 18.5) {
+      return BmiCategory.UNDERWEIGHT;
+    } else if (bodyMassIndex() > 25) {
+      return BmiCategory.OVERWEIGHT;
+    } else {
+      return BmiCategory.NORMAL;
+    }
+  }
+
+  public boolean isThinnerThan(BodyMass bm) {
+    return (this.bodyMassIndex() < bm.bodyMassIndex());
+  }
+
+  public double getWeight() {
+    return weight;
+  }
+
+  public double getHeight() {
+    return height;
+  }
+}
