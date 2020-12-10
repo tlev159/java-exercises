ccc134b6cf2a11ea4b9f4b9c7b03a16f4e5ac213
tlev159 <t.levente1598@gmail.com>
2020-12-10 15:45:27 +0100

    Lab without tests

---
+++ src/main/java/week07d04/Lab.java
@@ -0,0 +1,49 @@
+package week07d04;
+
+import java.time.LocalDate;
+
+public class Lab {
+
+  private String title;
+  private boolean completed;
+  private LocalDate completedAt;
+
+  public Lab(String title) {
+    this.title = title;
+    this.completed = false;
+  }
+
+  public Lab(String title, LocalDate completedAt) {
+    this.title = title;
+    this.completed = true;
+    this.completedAt = completedAt;
+  }
+
+  public void complete(LocalDate date) {
+    this.completed = true;
+    this.completedAt = date;
+  }
+
+  public void complete() {
+    this.completed = true;
+    this.completedAt = LocalDate.now();
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public boolean isCompleted() {
+    return completed;
+  }
+
+  public LocalDate getCompletedAt() {
+    return completedAt;
+  }
+
+  @Override
+  public String toString() {
+    return title + "(completed?: " + completed + "), completed at:" + completedAt +
+            '}';
+  }
+}
