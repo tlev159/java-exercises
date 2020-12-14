96c17bbd4e9f215f43047978422d25223e3fb733
tlev159 <t.levente1598@gmail.com>
2020-12-14 06:44:26 +0100

    Project 3 (Librarycatalog): add interface (Features) and AudioFeatures to save audiotitles

---
+++ src/main/java/catalog/AudioFeatures.java
@@ -0,0 +1,61 @@
+package catalog;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AudioFeatures implements Feature{
+
+  private List<String> composer = new ArrayList<>();
+  private int length;
+  private List<String> performers = new ArrayList<>();
+  private String title;
+
+  public AudioFeatures(String title, int length, List<String> performers) {
+    if (title.isBlank()) {
+      throw new IllegalArgumentException("Title must be not empty or null!");
+    }
+    this.title = title;
+    if (length < 1) {
+      throw new IllegalArgumentException("Length is not valid!");
+    }
+    this.length = length;
+    if (performers.isEmpty()) {
+      throw new IllegalArgumentException("Performerlist is not valid!");
+    }
+    this.performers = performers;
+  }
+
+  public AudioFeatures(String title, int length, List<String> performers, List<String> composer) {
+    if (title.isBlank()) {
+      throw new IllegalArgumentException("Title must be not empty or null!");
+    }
+    this.title = title;
+    if (length < 1) {
+      throw new IllegalArgumentException("Length is not valid!");
+    }
+    this.length = length;
+    if (performers.isEmpty()) {
+      throw new IllegalArgumentException("Performerlist is not valid!");
+    }
+    this.performers = performers;
+    this.composer = composer;
+  }
+
+  public int getLength() {
+    return length;
+  }
+
+  @Override
+  public List<String> getContributors() {
+    List<String> composersAndPerformers = new ArrayList<>();
+    composersAndPerformers.addAll(composer);
+    composersAndPerformers.addAll(performers);
+    return composersAndPerformers;
+  }
+
+  @Override
+  public String getTitle() {
+    return title;
+  }
+
+}
---
+++ src/main/java/catalog/Feature.java
@@ -0,0 +1,12 @@
+package catalog;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface Feature {
+
+  List<String> getContributors();
+
+  String getTitle();
+
+}
