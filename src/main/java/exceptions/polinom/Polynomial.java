23297c65ed519c4038d854668555c7c44707b021
levee98 <leventet@t-email.hu>
2021-01-03 06:18:49 +0100

    exceptions solution, but the given tests are bugged

---
+++ src/main/java/exceptions/polinom/Polynomial.java
@@ -0,0 +1,53 @@
+package exceptions.polinom;
+
+public class Polynomial {
+
+  private double[] coefficients;
+//  private String[] polinom;
+
+  public Polynomial(double[] coefficients) {
+    if (coefficients == null) {
+      throw new NullPointerException("coefficients is null");
+    }
+    this.coefficients = coefficients;
+  }
+
+  public Polynomial(String[] polinom) {
+    if (polinom == null) {
+      throw new NullPointerException("coefficientStrs is null");
+    }
+    if (isParesableString(polinom)) {
+      double[] poli = new double[polinom.length];
+      for (int i = 0; i < polinom.length; i++) {
+        poli[i] = Integer.parseInt(polinom[i]);
+      }
+      this.coefficients = poli;
+    }
+  }
+
+  private boolean isParesableString(String[] str) {
+    try {
+
+      for (String s:str) {
+        int c = Integer.parseInt(s);
+      }
+      return true;
+    } catch (IllegalArgumentException iae) {
+      throw new IllegalArgumentException("Illegal coefficients, not a number", iae);
+    }
+  }
+
+  public double evaluate(double x) {
+    double result = 0.0;
+    for (double d:coefficients) {
+      result += d * x;
+    }
+    return result;
+  }
+
+  public double[] getCoefficients() {
+    return coefficients;
+  }
+
+
+}
