e7e8a80e863617079b9b1f4825a16e08a33a4ee0
levee98 <leventet@t-email.hu>
2021-01-05 16:19:50 +0100

    corrected and extended tests

--- src/test/java/week10d02/MaxTravelTest.java
+++ src/test/java/week10d02/MaxTravelTest.java
@@ -13,4 +13,15 @@ class MaxTravelTest {
     assertEquals(12, new MaxTravel().getMaxIndex(List.of(12, 12, 0, 1, 3, 5, 7, 6, 7, 12)));
     assertEquals(4, new MaxTravel().getMaxIndex(List.of(12, 12, 0, 3, 4, 4)));
   }
+
+  @Test
+  void getNullListError() {
+    assertThrows(IllegalArgumentException.class, () -> new MaxTravel().getMaxIndex(null));
+  }
+
+  @Test
+  void getNullSizeListError() {
+    assertThrows(IllegalArgumentException.class, () -> new MaxTravel().getMaxIndex(List.of()));
+  }
+
 }
