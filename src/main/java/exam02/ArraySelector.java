3f61ce84e37c0df62b95667b21fabc0afa60ff3d
levee98 <leventet@t-email.hu>
2021-01-06 17:45:04 +0100

    exam02 select even indexed items from an array and return with the given concat

--- src/main/java/exam02/ArraySelector.java
+++ src/main/java/exam02/ArraySelector.java
@@ -1,4 +1,19 @@
 package exam02;
 
 public class ArraySelector {
+
+  public String selectEvens(int[] ints) {
+    if (ints.length == 0) {
+      return "";
+    }
+    StringBuilder sb = new StringBuilder();
+    sb.append("[");
+    for (int i = 0; i < ints.length; i += 2) {
+      sb.append(ints[i]).append(", ");
+    }
+    sb.delete(sb.length() - 2, sb.length());
+    sb.append("]");
+    return sb.toString();
+  }
+
 }
--- src/test/java/exam02/ArraySelectorTest.java
+++ src/test/java/exam02/ArraySelectorTest.java
@@ -6,13 +6,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ArraySelectorTest {
 
-//  @Test
-//  public void selectEvens() {
-//    assertEquals("", new ArraySelector().selectEvens(new int[]{}));
-//    assertEquals("[1]", new ArraySelector().selectEvens(new int[]{1}));
-//    assertEquals("[1]", new ArraySelector().selectEvens(new int[]{1, 2}));
-//    assertEquals("[1, 3]", new ArraySelector().selectEvens(new int[]{1, 2, 3}));
-//    assertEquals("[1, 3]", new ArraySelector().selectEvens(new int[]{1, 2, 3, 4}));
-//    assertEquals("[1, 3, 5]", new ArraySelector().selectEvens(new int[]{1, 2, 3, 4, 5}));
-//  }
+  @Test
+  public void selectEvens() {
+    assertEquals("", new ArraySelector().selectEvens(new int[]{}));
+    assertEquals("[1]", new ArraySelector().selectEvens(new int[]{1}));
+    assertEquals("[1]", new ArraySelector().selectEvens(new int[]{1, 2}));
+    assertEquals("[1, 3]", new ArraySelector().selectEvens(new int[]{1, 2, 3}));
+    assertEquals("[1, 3]", new ArraySelector().selectEvens(new int[]{1, 2, 3, 4}));
+    assertEquals("[1, 3, 5]", new ArraySelector().selectEvens(new int[]{1, 2, 3, 4, 5}));
+  }
 }
