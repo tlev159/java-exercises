6b591ebba0250747f54529c5800270170666daba
levee98 <leventet@t-email.hu>
2021-01-06 17:47:59 +0100

    exam02 Cv Skill class

--- src/test/java/exam02/cv/SkillTest.java
+++ src/test/java/exam02/cv/SkillTest.java
@@ -6,10 +6,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SkillTest {
 
-//  @Test
-//  public void testCreate() {
-//    Skill skill = new Skill("programming", 3);
-//    assertEquals("programming", skill.getName());
-//    assertEquals(3, skill.getLevel());
-//  }
+  @Test
+  public void testCreate() {
+    Skill skill = new Skill("programming", 3);
+    assertEquals("programming", skill.getName());
+    assertEquals(3, skill.getLevel());
+  }
 }
