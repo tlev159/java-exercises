d3bb07223adbb874bc03e47fd71d3a2a39b8364c
levee98 <leventet@t-email.hu>
2021-01-08 22:08:24 +0100

    4th project activity with track class

---
+++ src/main/java/activity/ActivityWithTrack.java
@@ -0,0 +1,23 @@
+package activity;
+
+public class ActivityWithTrack implements Activity {
+
+  private ActivityType activityType;
+
+  private Track track;
+
+  public ActivityWithTrack(Track track, ActivityType activityType) {
+    this.track = track;
+    this.activityType = activityType;
+  }
+
+  @Override
+  public double getDistance() {
+    return track.getDistance();
+  }
+
+  @Override
+  public ActivityType getType() {
+    return activityType;
+  }
+}
