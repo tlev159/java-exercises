055f6c606b5ea5452429bf2cea593296bc694bef
tlev159 <t.levente1598@gmail.com>
2021-01-21 14:48:40 +0100

    decoded secret bytes

---
+++ src/main/java/week12d04/Secret.java
@@ -0,0 +1,30 @@
+package week12d04;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Secret {
+
+  private static final String SEPARATOR = FileSystems.getDefault().getSeparator();
+
+  public void readSecretBinary(String pathString) {
+    String pathOfFile = "src" + SEPARATOR + "main" + SEPARATOR + "resources" + SEPARATOR + pathString;
+    Path file = Path.of(pathOfFile);
+    try {
+      byte[] bytes = Files.readAllBytes(file);
+      for (Byte b:bytes) {
+        System.out.print((char)(b + 10));
+      }
+    }
+    catch (IOException i) {
+      throw new IllegalStateException("Can not read binary file!", i);
+    }
+  }
+
+  public static void main(String[] args) {
+    new Secret().readSecretBinary("secret.dat");
+
+  }
+}
