de5d68818d25f6a8f09333b5a1f7680835b15f52
tlev159 <t.levente1598@gmail.com>
2021-02-07 15:47:43 +0100

    Collection map entry - logentry

---
+++ src/main/java/collectionsmap/Entry.java
@@ -0,0 +1,37 @@
+package collectionsmap;
+
+import java.time.LocalDate;
+
+public class Entry {
+
+  private String ipAddress;
+  private LocalDate entryDate;
+  private String login;
+
+  public Entry(String ipAddress, LocalDate entryDate, String login) {
+    this.ipAddress = ipAddress;
+    this.entryDate = entryDate;
+    this.login = login;
+  }
+
+  public String getIpAddress() {
+    return ipAddress;
+  }
+
+  public LocalDate getEntryDate() {
+    return entryDate;
+  }
+
+  public String getLogin() {
+    return login;
+  }
+
+  @Override
+  public String toString() {
+    return "Entry{" +
+            "ipAddress='" + ipAddress + '\'' +
+            ", entryDate=" + entryDate +
+            ", login='" + login + '\'' +
+            '}';
+  }
+}
---
+++ src/main/java/collectionsmap/LogParser.java
@@ -0,0 +1,49 @@
+package collectionsmap;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.*;
+
+public class LogParser {
+
+  public static final int NUMBER_OF_FIELDS = 3;
+  public static final int IP_POSITION = 0;
+  public static final int DATE_POSITION = 1;
+  public static final int LOGIN_POSITION = 2;
+  public static final String SEPARATOR = ":";
+  public static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+
+  public Map<String, List<Entry>> parseLog(String log) {
+    try (Scanner scanner = new Scanner(log)) {
+      Map<String, List<Entry>> result = new HashMap<>();
+      while (scanner.hasNext()) {
+        String line = scanner.next();
+        String[] splittedLog = line.split(SEPARATOR);
+        if (splittedLog.length != NUMBER_OF_FIELDS) {
+          throw new IllegalArgumentException("Incorrect log: incorrect number of fields");
+        }
+        String ip = splittedLog[IP_POSITION];
+        LocalDate date = stringToDate(splittedLog[DATE_POSITION]);
+        String login = splittedLog[LOGIN_POSITION];
+        Entry entry = new Entry(ip, date, login);
+        if (!result.containsKey(entry.getIpAddress())) {
+          result.put(entry.getIpAddress(), new ArrayList<>());
+        }
+        result.get(entry.getIpAddress()).add(entry);
+      }
+      return result;
+    }
+  }
+
+  private LocalDate stringToDate(String str) {
+    try {
+      return LocalDate.parse(str, DATE_FORMAT);
+    } catch (DateTimeParseException dtpe) {
+      throw new IllegalArgumentException("Incorrect log: incorrect date", dtpe);
+    }
+  }
+
+
+}
