99ed196706a2d50fd48965284c4217b078169bee
tlev159 <t.levente1598@gmail.com>
2021-02-24 22:00:13 +0100

    create track_point table

--- src/main/resources/db/migration/V1__activitytracker.sql
+++ src/main/resources/db/migration/V1__activitytracker.sql
@@ -1,1 +1,13 @@
-CREATE TABLE activities (id INT AUTO_INCREMENT PRIMARY KEY, start_time TIMESTAMP, activity_desc VARCHAR(255), activity_type VARCHAR(30));
+CREATE TABLE activities (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    start_time TIMESTAMP,
+    activity_desc VARCHAR(255),
+    activity_type VARCHAR(30));
+
+CREATE TABLE track_point (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+	time TIMESTAMP,
+	lat DECIMAL(8,4),
+	lon DECIMAL(8,4),
+	activity_id BIGINT NOT NULL,
+	FOREIGN KEY fk_id (activity_id) REFERENCES activities(id) ON UPDATE SET DEFAULT);
