a32357d000134168dc7bc1dd76d73835e27989c9
tlev159 <t.levente1598@gmail.com>
2021-03-03 06:34:13 +0100

    delete some lines

--- src/test/java/exam03retake02/TestTodoList.java
+++ src/test/java/exam03retake02/TestTodoList.java
@@ -3,6 +3,7 @@ package exam03retake02;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
