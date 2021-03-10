97d12a1723a98fe1c0925572b7d93b527922169b
tlev159 <t.levente1598@gmail.com>
2021-03-10 20:32:54 +0100

    week07d02 senior

---
+++ src/main/java/week07d02/User.java
@@ -0,0 +1,13 @@
+package week07d02;
+
+public interface User {
+
+  String getUserName();
+
+  String getFirstName();
+
+  String getLastName();
+
+  String getFullName();
+
+}
---
+++ src/main/java/week07d02/UserImpl.java
@@ -0,0 +1,35 @@
+package week07d02;
+
+public class UserImpl implements User {
+
+  private String userName;
+  private String firstName;
+  private String lastName;
+
+  public UserImpl(String userName, String firstName, String lastName) {
+    this.userName = userName;
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+
+  @Override
+  public String getUserName() {
+    return userName;
+  }
+
+  @Override
+  public String getFirstName() {
+    return firstName;
+  }
+
+  @Override
+  public String getLastName() {
+    return lastName;
+  }
+
+  @Override
+  public String getFullName() {
+    return firstName + " " + lastName;
+  }
+}
